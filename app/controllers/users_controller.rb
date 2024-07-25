441d64f3dbd1c619c1eac4ed74dba562747dbfab
nadraharr <nadraharr@gmail.com>
2024-07-26 01:31:35 +0300

    trackable, lockable

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,26 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    @users = User.all.order(:id)
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def ban
+    @user = User.find(params[:id])
+    if @user.access_locked?
+      @user.unlock_access!
+    else
+      @user.lock_access!
+    end
+
+    redirect_to users_path, notice: "User #{@user.id} #{"un" if !@user.access_locked?}banned!"
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to root_path, notice: "User destroyed!"
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,3 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+User.create(email: "admin@test.com", password: "qweqwe", password_confirmation: "qweqwe")
+User.create(email: "user@test.com", password: "qweqwe", password_confirmation: "qweqwe")
+User.update_all confirmed_at: DateTime.now
